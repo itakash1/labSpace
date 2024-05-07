0a60c4398e4e55eeadf422b14fee434aad7d9acb
itakash1 <itakashh11@gmail.com>
2024-05-08 01:54:52 +0400

    add lab02 in on4et

--- assembler/lab02/lab02/lab02.cpp
+++ assembler/lab02/lab02/lab02.cpp
@@ -1,6 +1,4 @@
-﻿//var:118
-
-#include <iostream>
+﻿#include <iostream>
 #include<cstdio>
 
 using namespace std;
@@ -18,12 +16,12 @@ int systemOfEquations(int a, int b) {
 		jl grp_c		// переход если a меньше b * переход если меньше JL
 
 		grp_a :				// a = b
-		imul ebx, 5		// b * 5
+			imul ebx, 5		// b * 5
 			mov eax, ebx	// eax <-- b * 5
 			jmp go_out		// переместиться к выводу
 
-			grp_b :				// a > b
-		mov eax, ebx	// eax <-- b
+		grp_b :				// a > b
+			mov eax, ebx	// eax <-- b
 			cdq				// eax = edx:eax
 			sub ecx, 9		// a - 9
 			cmp ecx, 9		// a ?= 9
@@ -31,18 +29,21 @@ int systemOfEquations(int a, int b) {
 			idiv ecx		// b / a - 9
 			jmp go_out		// переместиться к выводу
 
-			grp_c :				// a < b
-		imul ecx, ebx	// a * b
+		grp_c :				// a < b
+			imul ecx, ebx	// a * b
 			imul ecx, 7		// a * b * 7
 			sub ecx, 5		// a * b * 7 - 5
 			mov eax, ecx	// eax <-- a * b * 7 - 5
 			cdq				// eax = edx:eax
 			mov ecx, a		//	ecx <-- a
 			add ecx, ebx	// a + b
-			idiv ecx		// (a * b * 7 - 5)/(a + b)
+			cmp ecx, 0
+			je error_zero
+			ecx		// (a * b * 7 - 5)/(a + b)
 			jmp go_out		// переместиться к выводу
 
-			error_zero :
+		error_zero :
+			mov eax, -1
 
 	go_out:
 		mov result, eax	// result <-- eax
@@ -50,8 +51,20 @@ int systemOfEquations(int a, int b) {
 	return result;
 }
 
+#include <iostream>
+
+void printLabInfo() {
+	std::cout << "Ассемблер. Лабораторная работа № 2" << std::endl;
+	std::cout << "Выполнил ученик группы 6104, Маликов Николай Дмитриевич." << std::endl;
+	std::cout << "Вариант: 118" << std::endl;
+	std::cout << "Задание: x = b / (a - 9), если a > b" << std::endl;
+	std::cout << "Задание: x = -100, если a = b" << std::endl;
+	std::cout << "Задание: x = (6a * b - 5)/(a + b), если a < b" << std::endl;
+}
+
 int main() {
 	setlocale(LC_ALL, "Russian");
+	printLabInfo();
 
 	int a, b;
 
